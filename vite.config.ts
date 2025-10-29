2c54b381eee4e17c0e99ee0f34387fe0e3f6735a
CryptograssMan <78990681+CryptograssMan@users.noreply.github.com>
2025-10-29 13:34:13 +0800

    Add build configuration to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,14 @@ export default defineConfig({
     proxy: {
       '/api': 'http://127.0.0.1:8788'
     }
-  }
+  },
+  build: {
+    rollupOptions: {
+      // Don't bundle react or react-dom      
+      external: [
+        /^react(\/.*)?$/,
+        /^react-dom(\/.*)?$/
+      ],
+    },
+  },
 })
